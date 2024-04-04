9acd935020f3b6b99d2820f7de7ea16090f1d1da
heijsilva <profissionaljsilva@gmail.com>
2024-04-03 21:20:09 -0300

    Adicionado "Olá Mundo!"

---
+++ hello-world.cs
@@ -0,0 +1,12 @@
+using System,
+
+namespace Ola-mundo
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            console.writeline("Olá mundo!");
+        }
+    }
+}
